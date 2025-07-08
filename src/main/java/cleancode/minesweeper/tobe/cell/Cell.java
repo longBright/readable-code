8c4a28e4645f12ad382a1b95d7b18054a995a3dd
longbright <longbright0804@gmail.com>
2025-07-08 19:36:26 +0900

    refactor: 31.상속과 조합

--- src/main/java/cleancode/minesweeper/tobe/cell/Cell.java
+++ src/main/java/cleancode/minesweeper/tobe/cell/Cell.java
@@ -1,32 +1,21 @@
 package cleancode.minesweeper.tobe.cell;
 
-public abstract class Cell {
+public interface Cell {
 
-    protected static final String FLAG_SIGN = "⚑";
-    protected static final String UNCHECKED_SIGN = "□";
+    String FLAG_SIGN = "⚑";
+    String UNCHECKED_SIGN = "□";
 
-    protected boolean isFlagged;
-    protected boolean isOpened;
+    void flag();
 
-    public void flag() {
-        isFlagged = true;
-    }
+    void open();
 
-    public void open() {
-        isOpened = true;
-    }
+    boolean isChecked();
 
-    public boolean isChecked() {
-        return isFlagged || isOpened;
-    }
+    boolean isOpened();
 
-    public abstract boolean isLandMine();
+    boolean isLandMine();
 
-    public boolean isOpened() {
-        return isOpened;
-    }
+    boolean hasLandMineCount();
 
-    public abstract boolean hasLandMineCount();
-
-    public abstract String getSign();
+    String getSign();
 }
